0878405223c7467814539771c6b009f34861c207
katiePDX <61799774+katiepdx@users.noreply.github.com>
2020-10-20 16:20:47 -0700

    refactor ListPage

--- src/components/app/ListPage.jsx
+++ src/components/app/ListPage.jsx
@@ -2,14 +2,51 @@ import React, { Component } from 'react'
 import { getCharacters } from './services/characters-api'
 
 export default class ListPage extends Component {
-  async render() {
-    // api call - get all names
+  state = {
+    allCharacterInfo: [],
+    allNames: []
+  }
+  
+  async componentDidMount() {
+    // fetch character data on mount 
     const characters = await getCharacters();
-    console.log(characters)
-    
+
+    // map character details and grab the names
+    const characterNames = characters.map(character => { 
+      return character.name;
+    })
+  
+    // set characterDetails and characterNames to state
+    this.setState({
+      allCharacterInfo: characters, 
+      allNames: characterNames 
+    })
+
+    //Check data is in state
+    console.log('NAMES IN STATE', this.state.allNames)
+    console.log('DETAILS IN STATE', this.state.allCharacterInfo)
+  }
+
+   render() {
+    //  deconstruct state
+    const { allCharacterInfo, allNames } = this.state;
+
+    // mapped characters 
+    const mappedNames = allCharacterInfo.map(character => (
+      <div>
+        <li key={character._id}>
+          <a href={character.image}>{character.name}</a>
+        </li>
+      </div>
+    ))
+
     return (
       <>
         <h1>All Characters</h1>
+        <section>
+          Hey Arnold Characters!
+          <ul>{mappedNames}</ul>
+        </section>
       </>
     )
   }
