63f0f5c1b37f7592e7855fd61d9a9708d9e69256
katiePDX <61799774+katiepdx@users.noreply.github.com>
2020-10-20 23:37:52 -0700

    update test

--- src/components/app/App.test.jsx
+++ src/components/app/App.test.jsx
@@ -1,14 +1,17 @@
-// import React from 'react';
-// import { render, cleanup } from '@testing-library/react';
-// import App from './App';
+import '@babel/polyfill'
+import '@testing-library/jest-dom/extend-expect'
+import React from 'react';
+import { render, cleanup, waitFor } from '@testing-library/react';
+
+import App from './App';
 
 describe('App component', () => {
-  // afterEach(() => cleanup());
-  // it('renders App', () => {
-  //   const { asFragment } = render(<App />);
-  //   expect(asFragment()).toMatchSnapshot();
-  // });
-  it('renders App', () => {
-    expect('yes').toEqual('yes')
+  afterEach(() => cleanup());
+  it('renders App', async() => {
+    const { asFragment } = render(<App />);
+    
+    await waitFor(() => {
+      expect(asFragment()).toEqual(expect.any(DocumentFragment));
+    })
   });
 });
